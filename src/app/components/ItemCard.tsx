863c4450fb3adc365b6440c7e4492d06c22d4df0
Ajanan <ajanankumariah7@gmail.com>
2025-08-11 18:50:24 +0100

    added reveal animations and logic

--- src/app/components/ItemCard.tsx
+++ src/app/components/ItemCard.tsx
@@ -5,9 +5,15 @@ type ItemCardProps = {
   };
   onClick?: () => void;
   disabled?: boolean;
+  highlight?: "correct" | "wrong";
 };
 
-export default function ItemCard({ item, onClick, disabled }: ItemCardProps) {
+export default function ItemCard({
+  item,
+  onClick,
+  disabled,
+  highlight,
+}: ItemCardProps) {
   const { name, imageUrl } = item;
 
   return (
@@ -15,6 +21,15 @@ export default function ItemCard({ item, onClick, disabled }: ItemCardProps) {
       onClick={disabled ? undefined : onClick}
       className={`w-[400px] h-[700px] border-2 border-gray-600 flex flex-col rounded-2xl overflow-hidden transition-opacity duration-200 ${
         disabled ? "opacity-50 pointer-events-none cursor-not-allowed" : ""
+      } ${
+        highlight === "correct"
+          ? // green glow
+            "ring-2 ring-green-500/80 ring-offset-1 ring-offset-neutral-900 shadow-[0_0_38px_rgba(34,197,94,0.65)] transition-shadow"
+          : highlight === "wrong"
+          ? // red glow
+            "ring-2 ring-red-500/80 ring-offset-1 ring-offset-neutral-900 shadow-[0_0_38px_rgba(239,68,68,0.65)] transition-shadow"
+          : ""
+      }
       }`}
     >
       <div className="flex-grow w-full bg-gray-800" />
