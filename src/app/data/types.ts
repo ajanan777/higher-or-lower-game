7a1f444a05fd5ecb05e41c8efc5579946e9c0237
Ajanan <ajanankumariah7@gmail.com>
2025-08-11 18:48:34 +0100

    added new type revealstate type and guessresponse type

--- src/app/data/types.ts
+++ src/app/data/types.ts
@@ -8,3 +8,17 @@ export type Pair = {
     first: Item;
     second: Item
 }
+
+export type GuessResponse = {
+    leftRating: number;
+    rightRating: number;
+    chosenRating: number;
+    outcome: boolean
+}
+
+export type RevealState = {
+  chosenId: string;
+  outcome: boolean;
+  leftRating: number;
+  rightRating: number;
+};
