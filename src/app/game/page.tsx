3a17f53fb116dd199da69396aa08c62b51bf3d28
Ajanan <ajanankumariah7@gmail.com>
2025-08-11 18:50:08 +0100

    added reveal logic, added sleep timer for delays

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,48 +2,83 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import ItemCard from "../components/ItemCard";
-import { Item, Pair } from "../data/types";
+import { Item, Pair, GuessResponse, RevealState } from "../data/types";
 
 export default function Game() {
+  // delay helper function
+  const sleep = (ms: number) =>
+    new Promise<void>((resolve) => setTimeout(resolve, ms));
+
   const [pair, setPair] = useState<Pair | null>(null);
   const [isGuessing, setIsGuessing] = useState(false);
+
+  const [reveal, setReveal] = useState<RevealState | null>(null);
 
   const fetchPair = async () => {
     const response = await fetch("/api/pair");
     const data = await response.json();
     setPair(data);
+    setReveal(null);
   };
 
   useEffect(() => {
     fetchPair();
   }, []);
 
-  const yeah = async () => {};
-
-  const handleClick = async (item: Item) => {
+  const handleClick = async (chosenItem: Item, otherItem: Item) => {
     if (!pair || isGuessing) {
       console.log(isGuessing);
       return;
     }
     setIsGuessing(true);
+    await sleep(300);
     try {
       const response = await fetch("/api/guess", {
         method: "POST", //HTTP method
         headers: {
-          "Content-Type": "application/json", //telling backend i am sending JSON
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           leftID: pair.first.id,
           rightID: pair.second.id,
-          chosenID: item.id,
+          chosenID: chosenItem.id,
         }),
       });
       const data = await response.json();
-      console.log(data);
+      outcomeReveal(data, chosenItem, otherItem);
     } finally {
       console.log("guess complete");
       setIsGuessing(false);
     }
+  };
+
+  const outcomeReveal = async (
+    data: GuessResponse,
+    chosenItem: Item,
+    otherItem: Item
+  ) => {
+    const { chosenRating, leftRating, outcome, rightRating } = data;
+    console.log("outcome", chosenRating);
+
+    setReveal({
+      chosenId: chosenItem.id,
+      outcome,
+      leftRating,
+      rightRating,
+    });
+
+    if (outcome === true) {
+      //color cards green,(later: fetch score update)
+      console.log("(outcomeReveal)guess right");
+    }
+    if (outcome === false) {
+      console.log("(outcomeReveal)guess wrong");
+      //color cards red
+    }
+    //update score number
+    //get new pair cards
+    await sleep(1200);
+    fetchPair();
   };
 
   return (
@@ -53,15 +88,29 @@ export default function Game() {
           <div className={!isGuessing ? "cursor-pointer" : ""}>
             <ItemCard
               item={pair.first}
-              onClick={() => handleClick(pair.first)}
-              disabled={isGuessing}
+              onClick={() => handleClick(pair.first, pair.second)}
+              disabled={isGuessing || reveal?.chosenId === pair.second.id}
+              highlight={
+                reveal && reveal.chosenId === pair.first.id
+                  ? reveal.outcome
+                    ? "correct"
+                    : "wrong"
+                  : undefined
+              }
             />
           </div>
           <div className={!isGuessing ? "cursor-pointer" : ""}>
             <ItemCard
               item={pair.second}
-              onClick={() => handleClick(pair.second)}
-              disabled={isGuessing}
+              onClick={() => handleClick(pair.second, pair.first)}
+              disabled={isGuessing || reveal?.chosenId === pair.first.id}
+              highlight={
+                reveal && reveal.chosenId === pair.second.id
+                  ? reveal.outcome
+                    ? "correct"
+                    : "wrong"
+                  : undefined
+              }
             />
           </div>
         </div>
