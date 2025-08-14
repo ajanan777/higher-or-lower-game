d5c9f2304eaa38cd56086aee42ac738840f9e844
Ajanan <ajanankumariah7@gmail.com>
2025-08-14 16:03:20 +0100

    added image, changed dimesions and styling

--- src/app/components/ItemCard.tsx
+++ src/app/components/ItemCard.tsx
@@ -18,11 +18,13 @@ export default function ItemCard({
 }: ItemCardProps) {
   const { name, imageUrl } = item;
   const nameLength = name.length;
+  const scale = 240;
 
   return (
     <div
       onClick={disabled ? undefined : onClick}
-      className={`relative w-[400px] h-[700px] border-2 flex flex-col rounded-2xl overflow-hidden transition-opacity duration-200 ${
+      style={{ width: 2 * scale, height: 3 * scale }}
+      className={`relative border-2 flex flex-col rounded-2xl overflow-hidden transition-opacity duration-200 ${
         disabled ? "opacity-50 pointer-events-none cursor-not-allowed" : ""
       } 
       ${highlight === "wrong" ? "border-[#ff053b]" : "border-[#05ffa1]"}  
@@ -34,15 +36,34 @@ export default function ItemCard({
           : "transition-transform duration-500 ease-in-out hover:-translate-y-1 hover:scale-102"
       }`}
     >
+      {/* IMAGE AREA */}
+      <div className="relative flex-grow w-full overflow-hidden flex items-center justify-center pointer-events-none">
+        {/* background (behind) */}
+        <img
+          src={imageUrl}
+          alt=""
+          aria-hidden="true"
+          className="absolute inset-0 w-full h-full object-cover scale-105 blur-[3px] opacity-50 z-0 pointer-events-none"
+        />
+        {/* foreground (sharp) */}
+        <img
+          src={imageUrl}
+          alt={name}
+          className="relative z-10 block w-full h-auto"
+          loading="lazy"
+        />
+      </div>
+
+      {/* RATING OVERLAY*/}
       {typeof rating === "number" && (
-        <div className="absolute inset-0 flex items-center justify-center pointer-events-none animate-pulse">
+        <div className="absolute inset-0 z-20 flex items-center justify-center pointer-events-none">
           <span
-            className={`text-6xl font-extrabold ] ${
+            className={`text-6xl font-extrabold drop-shadow-[0_0_8px_rgba(0,0,0,0.7)] ${
               highlight === "correct"
                 ? "text-green-500"
                 : highlight === "wrong"
                 ? "text-red-500"
-                : "text-green-500 [text-shadow:0_0_6px_#22c55e,0_0_2px_#22c55e]"
+                : "text-green-500"
             }`}
           >
             {rating}
@@ -50,8 +71,7 @@ export default function ItemCard({
         </div>
       )}
 
-      <div className="flex-grow w-full bg-[#23064e]" />
-      <div className="h-[60px] w-full bg-[#170435] flex items-center justify-center">
+      <div className="h-[60px] w-full bg-[#351353] flex items-center justify-center">
         <span
           className={`text-[#00dffc] font-semibold text-center ${
             nameLength > 50 ? "" : "text-xl"
